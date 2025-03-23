9480e5b2d2b968734d35ff8a588867f425f35a58
CatWithCode <56090976+CatWithCode@users.noreply.github.com>
2025-03-23 17:56:25 +0100

    Update MoonlightStarter.sh

--- GameMode/MoonlightStarter.sh
+++ GameMode/MoonlightStarter.sh
@@ -4,12 +4,20 @@
 sleep 1
 killall swayidle
 
+# Disable EyeCandy:
 sleep 1
+bash ~/.config/hypr/Scripts/GameMode/GameMode.sh
 
 # Run Moonlight:
+sleep 1
 flatpak run com.moonlight_stream.Moonlight
 
+# Return to Default:
 killall swayidle
+sleep1
+
+bash ~/.config/hypr/Scripts/GameMode/GameMode.sh notReset
+
 bash ~/.config/hypr/Scripts/IDLE.sh &> /dev/null & disown %-
 
 sleep 1
